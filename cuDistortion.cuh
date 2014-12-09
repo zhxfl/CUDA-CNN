8335fa3ae46edeba674d6a884920f4a88627fd75
yarn <yarn@master.ustcsz.edu.cn>
2014-12-09 22:32:40 +0800

    remove old files

--- cuDistortion.cuh
+++
@@ -1,9 +0,0 @@
-#ifndef _CU_DISTORTION_H_
-#define _CU_DISTORTION_H_
-
-void cuInitDistortionMemery(int batch, int ImgSize);
-void cuApplyRandom(int batch, unsigned long long s, double dElasticScaling, int ImgSize);
-void cuApplyDistortion(double**inputs, double**outputs, int batch, int ImgSize);
-void cuApplyCrop(double**inputs, double**outputs, int batch,int ImgSize);
-
-#endif
