998d5010011338fdeddd9f840a69a4ec7990a18f
zhxfl <291221622@qq.com>
2015-01-20 20:07:07 +0800

    mkdir for readdata files

---
+++ readData/readCIFAR10Data.h
@@ -0,0 +1,17 @@
+#ifndef _READ_CIFAR10_DATA_H_
+#define _READ_CIFAR10_DATA_H_
+
+#include "cuMatrix.h"
+#include <string>
+#include <vector>
+#include "util.h"
+#include "cuMatrixVector.h"
+
+/*read trainning data and lables*/
+void read_CIFAR10_Data(cuMatrixVector<double> &trainX,
+	cuMatrixVector<double>&testX,
+	cuMatrix<int>*&trainY,
+	cuMatrix<int>*&testY);
+
+
+#endif
