2ebc0a57cd0fbe53ee0564c73412eb7753046c1e
zhxfl <291221622@qq.com>
2015-04-25 20:52:34 +0800

    realease

--- Release/subdir.mk
+++ Release/subdir.mk
@@ -24,16 +24,16 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-6.5/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-6.5_Samples/common/inc/ -O3 -gencode arch=compute_50,code=sm_50  -odir "" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-6.5/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-6.5_Samples/common/inc/ -O3 --compile  -x c++ -o  "$@" "$<"
+	/usr/local/cuda-7.0/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-7.0_Samples/common/inc/ -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_50,code=sm_50  -odir "." -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-7.0/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-7.0_Samples/common/inc/ -O3 --compile  -x c++ -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 %.o: ../%.cu
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-6.5/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-6.5_Samples/common/inc/ -O3 -gencode arch=compute_50,code=sm_50  -odir "" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-6.5/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-6.5_Samples/common/inc/ -O3 --compile --relocatable-device-code=true -gencode arch=compute_50,code=compute_50 -gencode arch=compute_50,code=sm_50  -x cu -o  "$@" "$<"
+	/usr/local/cuda-7.0/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-7.0_Samples/common/inc/ -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_50,code=sm_50  -odir "." -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-7.0/bin/nvcc -I/usr/local/cuda/NVIDIA_CUDA-7.0_Samples/common/inc/ -O3 --compile --relocatable-device-code=true -gencode arch=compute_20,code=compute_20 -gencode arch=compute_50,code=compute_50 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_50,code=sm_50  -x cu -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
