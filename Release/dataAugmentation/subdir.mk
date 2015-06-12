ea5fda67a83bce6c28fea5212e35ef2112dfbfca
zhxfl <291221622@qq.com>
2015-06-12 11:00:25 +0800

    release makefile

---
+++ Release/dataAugmentation/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../dataAugmentation/cuTrasformation.cu \
+../dataAugmentation/dataPretreatment.cu \
+../dataAugmentation/pca.cu 
+
+CU_DEPS += \
+./dataAugmentation/cuTrasformation.d \
+./dataAugmentation/dataPretreatment.d \
+./dataAugmentation/pca.d 
+
+OBJS += \
+./dataAugmentation/cuTrasformation.o \
+./dataAugmentation/dataPretreatment.o \
+./dataAugmentation/pca.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+dataAugmentation/%.o: ../dataAugmentation/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-6.5/bin/nvcc -I/usr/local/cuda-6.5/NVIDIA_CUDA-6.5_Samples/common/inc/ -O3 -gencode arch=compute_50,code=sm_50  -odir "dataAugmentation" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-6.5/bin/nvcc -I/usr/local/cuda-6.5/NVIDIA_CUDA-6.5_Samples/common/inc/ -O3 --compile --relocatable-device-code=true -gencode arch=compute_50,code=compute_50 -gencode arch=compute_50,code=sm_50  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
