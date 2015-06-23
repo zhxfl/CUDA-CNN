7dfd6f78e8e1265f014a03156d82a1ac1bc4ac12
zhxfl <291221622@qq.com>
2015-06-23 19:40:27 +0800

    cmake

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_core -lopencv_imgproc -lcublas -lcurand -lopencv_highgui
-
